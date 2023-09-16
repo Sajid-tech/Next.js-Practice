58e4b171924d24a660b81d4c7c8b0e66b08eba09
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-09-16 15:06:35 +0530

    404 page done

---
+++ src/app/about/[...not-found]/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function AboutNotFound() {
+    return (
+        <div>
+            <h1>This About page is not availabel in website</h1>
+            <Link href="/">Go to Homepage</Link>
+        </div>
+    )
+}
